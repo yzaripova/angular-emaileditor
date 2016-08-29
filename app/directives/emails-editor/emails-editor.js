701106815fc4ef0b3a7a552fc12ea8c50a6bf10f
yzaripova <zaripovaya@gmail.com>
2016-08-29 22:36:09 +0500

    Fix styles, template & some improvements

--- app/directives/emails-editor/emails-editor.js
+++ app/directives/emails-editor/emails-editor.js
@@ -8,12 +8,22 @@
         scope: {
           emails: '='
         },
-        controller: function ($scope, $element, $attrs, $transclude, $window) {
+        controller: function ($scope, $element, $attrs, $transclude) {
           $scope.pattern = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i;
+
+          $scope.click = function() {
+            $('.emails-editor>input').focus();
+          };
 
           $scope.keyPress = function(event) {
             if (event.keyCode  === 13 || event.keyCode === 44) {
               event.preventDefault();
+              addEmailBlock($scope);
+            }
+          };
+
+          $scope.keyDown = function(event) {
+            if(event.ctrlKey && event.keyCode == 86){
               addEmailBlock($scope);
             }
           };
@@ -24,14 +34,14 @@
 
           $scope.removeEmails = function(emails, index) {
             emails.splice(index, 1);
-          }
+          };
 
           $scope.isValid = function(email) {
             var isValid = $scope.pattern.test(email);
             if (!isValid) {
               return 'not-valid';
             }
-          }
+          };          
         }
       }
     });
@@ -41,6 +51,6 @@
       $scope.emails.push($scope.text);
     }
 
-    $scope.text = "";
+    $scope.text = '';
   }
 })();
